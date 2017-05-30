ae46ee997985a11a9c46e2c3faf3e0ffe1d4d824
dam <dam@Aulaxx-yy>
2017-05-30 17:16:48 +0200

    Version 1.0

---
+++ src/gameobjects/Enemigo3.java
@@ -0,0 +1,21 @@
+package gameobjects;
+
+import java.io.IOException;
+
+/**
+ * Primer enemigo
+ * @author Noelia
+ */
+public class Enemigo3 extends Personaje{
+    
+    private static final String DIRECCION_IMAGEN = "XXX";
+    private static final int X_POSICION = 150;
+    private static final int Y_POSICION = 25;
+    private static final int VELOCIDAD = 9;
+    private static final int VIDAS = 1;
+    
+    public Enemigo3(String direccionImagen, int xPosicion, int yPosicion, int velocidad, int vidas) throws IOException {
+        super(DIRECCION_IMAGEN, X_POSICION, Y_POSICION, VELOCIDAD, VIDAS);
+    }
+    
+}
