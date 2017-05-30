4743b1cde41d61c85978fbccb02da136006d8f74
dam <dam@Aulaxx-yy>
2017-05-30 17:17:19 +0200

    personaje carlos (Enemigo 2)

---
+++ src/gameobjects/Enemigo2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameobjects;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Enemigo2 extends Personaje{
+    
+    private static final String DIRECCION_IMAGEN = "XXX";
+    private static final int X_POSICION = 80;
+    private static final int Y_POSICION = 45;
+    private static final int VELOCIDAD = 10;
+    private static final int VIDAS = 1;
+    
+    public Enemigo2(String direccionImagen, int xPosicion, int yPosicion, int velocidad, int vidas) throws IOException {
+        super(direccionImagen, xPosicion, yPosicion, velocidad, vidas);
+    }
+    
+    
+    
+}
