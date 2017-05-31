5e5b842463b907d59ced9ca937ff09213c304f0e
dam <dam@Aulaxx-yy>
2017-05-31 16:21:29 +0200

    Enemigos

--- src/gameobjects/Enemigo2.java
+++ src/gameobjects/Enemigo2.java
@@ -23,6 +23,11 @@ public class Enemigo2 extends Personaje{
         super(direccionImagen, xPosicion, yPosicion, velocidad, vidas);
     }
 
+    @Override
+    public void recibirImpacto() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     
     
 }
