56635ce1ec8e0ffaa607c0823adb4db0d4a344a1
dam <dam@Aulaxx-yy>
2017-06-01 17:23:28 +0200

    Cambio de sprite Enemigo1

--- src/gameobjects/Enemigo1.java
+++ src/gameobjects/Enemigo1.java
@@ -26,19 +26,36 @@ public class Enemigo1 extends Personaje {
     @Override
     public void run() {
         int estado = 1;
+        int i = 0;
         while (true) {
             try {
                 sleep(16);
                 if (estado == 1) {
                     xPosicion = xPosicion + 3;
+                    if (i == 0){
+                        this.sprite = ImageIO.read(JPGameScreen.class.getResourceAsStream("/assets/MaloJavi/MaloJaviDerE.png"));
+                    } else if(i == 5){
+                        this.sprite = ImageIO.read(JPGameScreen.class.getResourceAsStream("/assets/MaloJavi/MaloJaviDerQ.png"));
+                    } else if (i == 10){
+                        this.sprite = ImageIO.read(JPGameScreen.class.getResourceAsStream("/assets/MaloJavi/MaloJaviDerechaW.png"));
+                        i = -1;
+                    }
+                    i++;
                     if (xPosicion > width-25) {
-                        this.sprite = ImageIO.read(JPGameScreen.class.getResourceAsStream("/assets/MaloJavi/MaloJaviIzLado.png"));                        
                         estado = 2;
                     }
                 } else if (estado == 2) {
                     xPosicion = xPosicion - 3;
+                    if (i == 0){
+                        this.sprite = ImageIO.read(JPGameScreen.class.getResourceAsStream("/assets/MaloJavi/MaloJaviIzLado.png"));
+                    } else if(i == 5){
+                        this.sprite = ImageIO.read(JPGameScreen.class.getResourceAsStream("/assets/MaloJavi/MaloJaviIzLadoQ.png"));
+                    } else if (i == 10){
+                        this.sprite = ImageIO.read(JPGameScreen.class.getResourceAsStream("/assets/MaloJavi/MaloJaviIzW.png"));
+                        i = -1;
+                    }
+                    i++;
                     if (xPosicion < 0) {
-                        this.sprite = ImageIO.read(JPGameScreen.class.getResourceAsStream("/assets/MaloJavi/MaloJaviDerE.png"));
                         estado = 1;
                     }
                 }
