c2863ff5fe58f6b593f707a40090a859075a9540
dam <dam@Aulaxx-yy>
2017-06-01 16:36:48 +0200

    Movimiento Enemigo1

--- src/gameobjects/Enemigo1.java
+++ src/gameobjects/Enemigo1.java
@@ -1,13 +1,17 @@
 package gameobjects;
 
 import java.io.IOException;
+import static java.lang.Thread.sleep;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Primer enemigo
+ *
  * @author SabotenSama
  */
-public class Enemigo1 extends Personaje{
-    
+public class Enemigo1 extends Personaje {
+
     public Enemigo1(String direccionImagen, int xPosicion, int yPosicion, int velocidad, int vidas) throws IOException {
         super(direccionImagen, xPosicion, yPosicion, velocidad, vidas);
     }
@@ -19,7 +23,24 @@ public class Enemigo1 extends Personaje{
 
     @Override
     public void run() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int estado = 1;
+        while (true) {
+            try {
+                sleep(16);
+                if (estado == 1) {
+                    xPosicion = xPosicion + 3;
+                    if (xPosicion > 200) {
+                        estado = 2;
+                    }
+                } else if (estado == 2) {
+                    xPosicion = xPosicion - 3;
+                    if (xPosicion < 0) {
+                        estado = 1;
+                    }
+                }
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Enemigo1.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
-    
 }
