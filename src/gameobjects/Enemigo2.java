547189a22d9235390219e8b5f55829c57a9b63bd
dam <dam@Aulaxx-yy>
2017-06-05 18:15:29 +0200

    personaje carlos (Enemigo 2)

--- src/gameobjects/Enemigo2.java
+++ src/gameobjects/Enemigo2.java
@@ -9,13 +9,15 @@ import java.io.IOException;
 import static java.lang.Thread.sleep;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import net.juanxxiii.j23gameengine.gui.JPGameScreen;
 
 /**
  *
  * @author Carlos
  */
 public class Enemigo2 extends Personaje{
-    
+    private int estado=1;
     public Enemigo2(String direccionImagen, int xPosicion, int yPosicion) throws IOException {
         super(direccionImagen, xPosicion, yPosicion);
     }
@@ -27,19 +29,21 @@ public class Enemigo2 extends Personaje{
 
     @Override
     public void run() {
-        int estado=1;
+        
         while(true){
             try {
                 sleep(25);
                 if(estado==1){
                     xPosicion = xPosicion + 3;
-                    if (xPosicion > height) {
+                    if (xPosicion < 0) {
                         estado = 2;
+                        //this.sprite = ImageIO.read(JPGameScreen.class.getResourceAsStream("/assets/MaloNoe/Der1.png"));
                     }
                 }else if(estado==2){
                     xPosicion = xPosicion - 3;
                     if (xPosicion < 0) {
                         estado = 1;
+                        //this.sprite = ImageIO.read(JPGameScreen.class.getResourceAsStream("/assets/MaloNoe/Der1.png"));
                     }
                 }
                 
